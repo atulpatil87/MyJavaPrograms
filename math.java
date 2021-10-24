86ea49eedd42bd830b776ebfe0765c12544fd82d
priyesh069 <58878642+priyesh069@users.noreply.github.com>
2021-10-25 00:07:12 +0530

    Add files via upload

---
+++ math.java
@@ -0,0 +1,15 @@
+// addition multiplication division substraction mod
+class math
+{
+	public static void main(String[] args)
+	{
+		int a = 50;
+		int b = 30;
+		int add = a+b;
+		int sub = a-b;
+		float div = a/b;
+		int mul = a*b;
+		int mod = a%b;
+		System.out.print("Addition = "+add+"\nSubtraction = "+sub+"\nDivision = "+div+"\nMultiplication = "+mul+"\nRemainder = "+mod);
+	}
+}
