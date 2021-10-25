36f89340c8dcdd54e9042497b13f7ce4ed33ce44
priyesh069 <58878642+priyesh069@users.noreply.github.com>
2021-10-25 21:27:51 +0530

    Find the Area of circle

---
+++ circle.java
@@ -0,0 +1,11 @@
+//   calcuate the area of circle
+class circle
+{
+	public static void main(String[] args)
+	{
+	double pi=3.14;
+	int r=5;
+	double area=pi*r*r;
+	System.out.print("area of circle is "+area);
+	}
+}
