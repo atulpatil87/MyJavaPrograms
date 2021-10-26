e5ce7e40a6daf9471179cd8b769db640416168c9
priyesh069 <58878642+priyesh069@users.noreply.github.com>
2021-10-26 10:07:35 +0530

    Write a java program to calculate the area of rectangle
    Write a java program to calculate the area of rectangle.
    #hacktoberfest

---
+++ rectangle.java
@@ -0,0 +1,11 @@
+// calculate the are of rectangle
+class rectangle
+	{
+	public static void main(String[] args)
+		{
+			int length = 12;
+			int bredth = 28;
+			int area = length*bredth;
+			System.out.print("Area of rectangle is " +area);
+		}
+	}
