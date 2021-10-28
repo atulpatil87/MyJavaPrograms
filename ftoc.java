dedd4f96c06b5495a043c1c8ff780901d5bfa4b0
priyesh069 <58878642+priyesh069@users.noreply.github.com>
2021-10-28 20:03:20 +0530

    Fahrenheit to Celsius

---
+++ ftoc.java
@@ -0,0 +1,12 @@
+// Farenite to celcius
+class ftoc
+	{
+		public static void main(String[] args)
+		{
+			float F= 302.66f;
+			//float C= (9/5)*+32;
+			float C = F - 32f * (5/9f);
+			
+			System.out.print("\nThe Tempreture Is "+F+" Farenite And "+C+" Celcius");
+		}
+	}
