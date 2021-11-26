4aa72b38a487e47f85b3e894d7d02f6c16f94e6c
Atul Patil <58878642+priyesh069@users.noreply.github.com>
2021-11-26 23:28:07 +0530

    Swap The User Input Number

---
+++ SwapTheNumber.java
@@ -0,0 +1,30 @@
+package kunal;
+
+import java.security.DrbgParameters;
+import java.util.Scanner;
+
+import static java.lang.System.*;
+
+public class SwapTheNumber {
+
+    public SwapTheNumber(int a, int b,int c)
+    {
+        System.out.println(+a+"\n"+b);
+        int temp;
+        temp=a;
+        a=b;
+        b=c;
+        c=temp;
+        System.out.println("***************************");
+        System.out.println(+a+"\n"+b+"\n"+c);
+
+
+    }
+
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        new SwapTheNumber(10,20,30);
+
+    }
+}
