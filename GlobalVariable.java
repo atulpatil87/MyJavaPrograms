b1684a637c89269593b4c7e092571f578e1d30e2
Atul Patil <58878642+priyesh069@users.noreply.github.com>
2021-11-27 18:26:34 +0530

    Define the global variable

---
+++ GlobalVariable.java
@@ -0,0 +1,29 @@
+package kunal;
+
+public class GlobalVariable
+{
+    static byte a;
+    static char b;
+    static short c;
+    static int d;
+    static long e;
+    static float f;
+    static double g;
+    static boolean h;
+
+
+    public static void main(String[] args)
+    {
+        System.out.println("a="+a);
+        System.out.println("b="+b);
+        System.out.println("c="+c);
+        System.out.println("d="+d);
+        System.out.println("e="+e);
+        System.out.println("f="+f);
+        System.out.println("g="+g);
+        System.out.println("h="+h);
+
+
+
+    }
+}
