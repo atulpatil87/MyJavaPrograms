d902d4c76480ef559f7e0c97b1cb45ec00265da5
Atul Patil <58878642+priyesh069@users.noreply.github.com>
2022-06-14 14:13:02 +0530

    Update ArithmeticOperation.java

--- ArithmeticOperation.java
+++ ArithmeticOperation.java
@@ -5,10 +5,10 @@ class ArithmeticOperation
 	{
 		int a = 50;
 		int b = 30;
-		int add = a+b; // For Multiplication 
+		int add = a+b; 
 		int sub = a-b;
 		float div = a/b;
-		int mul = a*b;
+		int mul = a*b; // For Multiplication 
 		int mod = a%b;
 		System.out.print("Addition = "+add+"\nSubtraction = "+sub+"\nDivision = "+div+"\nMultiplication = "+mul+"\nRemainder = "+mod);
 	}
