723d2e397c0e23061f4ff6024b25e413f1e93145
Atul Patil <58878642+priyesh069@users.noreply.github.com>
2022-12-13 20:55:34 +0530

    new program

---
+++ Binary_search.java
@@ -0,0 +1,11 @@
+public class Binary_Search {
+    /* 1. For Binary Search first find the middle element of the array. if the arry is sorted
+       2. Check the target > mid, then search in right
+          else search in left
+          Error: If int mid= (start + end)/2 then
+                    int mid= start + (end-start)/2;
+                    
+
+     */
+
+}
